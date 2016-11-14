498cbe118c9ce4b71d8fd3cd124284ebaf786a97
NatasaPeic <peic.natasa@gmail.com>
2016-11-14 18:22:43 -0500

    Fix typo for review author

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -30,10 +30,11 @@
         this.review = {};
 
         this.addReview = function(product){
+          this.review = {'createdOn': Date.now()};
           product.reviews.push(this.review);
           this.review = {};
-        }
-      })
+        };
+      });
 
 
       // #1
