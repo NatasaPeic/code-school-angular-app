049bcea05c7d412013081272265ab80b36245334
NatasaPeic <peic.natasa@gmail.com>
2016-11-14 09:42:18 -0500

    Add controllrer

---
+++ assets/scripts/controllers/MainController.js
@@ -0,0 +1,49 @@
+// var app = require('../app');
+//
+//
+// app.controller('MainController', ['$scope', function($scope) {
+//   $scope.exercises = [
+//     {
+//       icon: 'http://i.imgur.com/uSNt3tH.jpg',
+//       name: 'Pushups',
+//       count: 20
+//     },
+//     {
+//       icon: 'http://i.imgur.com/uSNt3tH.jpg',
+//       name: 'Squats',
+//       count: 15
+//     },
+//     {
+//       icon: 'http://i.imgur.com/uSNt3tH.jpg',
+//       name: 'Pullups',
+//       count: 10
+//     },
+//     {
+//       icon: 'http://i.imgur.com/uSNt3tH.jpg',
+//       name: 'Rows',
+//       count: 10
+//     },
+//     {
+//       icon: 'http://i.imgur.com/uSNt3tH.jpg',
+//       name: 'Lunges',
+//       count: 10
+//     },
+//     {
+//       icon: 'http://i.imgur.com/uSNt3tH.jpg',
+//       name: 'Step Ups',
+//       count: 10
+//     },
+//     {
+//       icon: 'http://i.imgur.com/uSNt3tH.jpg',
+//       name: 'Sit Ups',
+//       count: 15
+//     }
+//   ];
+//   $scope.increase = function(index){
+//      $scope.exercises[index].count += 1;
+//    };
+//    $scope.decrease = function(index){
+//      $scope.exercises[index].count -= 1;
+//    };
+//
+// }]);
