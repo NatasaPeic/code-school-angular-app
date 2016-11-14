9056a633c11d3cfc3b2d74265a6c4e7a81065ad8
NatasaPeic <peic.natasa@gmail.com>
2016-11-14 09:55:42 -0500

    Using ng-repeat

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,15 +1,30 @@
-
 (function(){
 
   var app = angular.module('gemStore', []);
       app.controller('StoreController', function(){
-        this.product = gem;
+        // this.product = gem;
+        this.products = gems;
       });
 
-      var gem = {
-        name: 'Azurite',
-        price: 2.95,
-        canPurchase: false,
-        soldOut: true };
+      // var gem = {
+      //   name: 'Azurite',
+      //   price: 2.95,
+      //   canPurchase: true,
+      //   soldOut: true };
+
+      var gems = [
+        {
+          name: 'Azurite',
+          price: 2.95
+        },
+        {
+          name: 'Bloodstone',
+          price: 5.95
+        },
+        {
+          name: 'Zircon',
+          price: 3.95
+        }
+ ];
 
 })();
