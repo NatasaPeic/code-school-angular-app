f1ff2370362da66a15086894ce29c5bd8b145874
NatasaPeic <peic.natasa@gmail.com>
2016-11-14 12:36:37 -0500

    Add PanelController

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -11,7 +11,11 @@
 
         this.selectTab = function(setTab){
           this.tab = setTab;
-        }
+        };
+
+        this.isSelected = function(checkTab){
+          return this.tab === checkTab;
+        };
       });
 
 
