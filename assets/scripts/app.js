5a46bc0b85d5b8957864dd991185f601318708f1
NatasaPeic <peic.natasa@gmail.com>
2016-11-14 22:03:50 -0500

    Add new angular module and link it

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,6 @@
 (function(){
 
-  var app = angular.module('gemStore', []);
+  var app = angular.module('gemStore', ['store-directives']);
       app.controller('StoreController', function(){
         // this.product = gem;
         this.products = gems;
@@ -36,61 +36,63 @@
         };
       });
 
-      app.directive('productDescription', function(){
-       return {
-         restrict: 'E',
-         templateUrl: 'product-description.html'
-       };
-     });
 
-     app.directive('productSpecs', function(){
-        return{
-          restrict: 'A',
-          templateUrl: 'product-specs.html'
-        };
-      });
-
-      app.directive("productReviews", function() {
-        return {
-          restrict: 'E',
-          templateUrl: "product-reviews.html"
-        };
-      });
-
-      app.directive('productTabs', function(){
-         return {
-           restrict: 'E',
-           templateUrl: 'product-tabs.html',
-           controller: function(){
-             this.tab = 1;
-
-             this.selectTab = function(setTab){
-               this.tab = setTab;
-             };
-
-             this.isSelected = function(checkTab){
-               return this.tab === checkTab;
-             };
-           },
-              controllerAs: 'panel'
-           };
-
-       });
-
-       app.directive('productGallery', function(){
-          return {
-            restrict: 'E',
-            templateUrl: 'product-gallery.html',
-            controller: function(){
-              this.current = 0;
-
-              this.setCurrent = function(value) {
-                this.current = value || 0;
-              };
-            },
-            controllerAs: 'gallery'
-          };
-        });
+    // move them to the products.js
+    //   app.directive('productDescription', function(){
+    //    return {
+    //      restrict: 'E',
+    //      templateUrl: 'product-description.html'
+    //    };
+    //  });
+     //
+    //  app.directive('productSpecs', function(){
+    //     return{
+    //       restrict: 'A',
+    //       templateUrl: 'product-specs.html'
+    //     };
+    //   });
+     //
+    //   app.directive("productReviews", function() {
+    //     return {
+    //       restrict: 'E',
+    //       templateUrl: "product-reviews.html"
+    //     };
+    //   });
+     //
+    //   app.directive('productTabs', function(){
+    //      return {
+    //        restrict: 'E',
+    //        templateUrl: 'product-tabs.html',
+    //        controller: function(){
+    //          this.tab = 1;
+     //
+    //          this.selectTab = function(setTab){
+    //            this.tab = setTab;
+    //          };
+     //
+    //          this.isSelected = function(checkTab){
+    //            return this.tab === checkTab;
+    //          };
+    //        },
+    //           controllerAs: 'panel'
+    //        };
+     //
+    //    });
+     //
+    //    app.directive('productGallery', function(){
+    //       return {
+    //         restrict: 'E',
+    //         templateUrl: 'product-gallery.html',
+    //         controller: function(){
+    //           this.current = 0;
+     //
+    //           this.setCurrent = function(value) {
+    //             this.current = value || 0;
+    //           };
+    //         },
+    //         controllerAs: 'gallery'
+    //       };
+    //     });
 
       // #1
       // var gem = {
