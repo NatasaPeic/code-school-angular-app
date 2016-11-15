22543b48511fd47b5a418006ee2ea837a9be41b2
NatasaPeic <peic.natasa@gmail.com>
2016-11-14 22:52:58 -0500

    Add http service

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,6 +6,16 @@
         this.products = gems;
       });
 
+
+    // services will not work locally
+    //   app.controller('StoreController', ['$http', function($http){
+    //   var store = this;
+    //   store.products = [];
+    //
+    //    $http.get('/store-products.json').success(function(data){
+    //   store.products = data;
+    // }]);
+
       // app.controller('PanelController', function(){
       //   this.tab = 1;
       //
